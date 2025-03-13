e564be6e6a8da76257689bd0e3473f6cfd57a46a
HarjotSahota <93726984+HarjotSahota@users.noreply.github.com>
2025-03-12 19:20:57 -0700

    Updated UI for count
    when a player collects a coin there is an animation where it updates the score by 1

--- Assets/_Scripts/CoinCounterUI.cs
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class CoinCounterUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration = 0.4f;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+    private void Start()
     {
-        
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScore(int score)
     {
-        
+        // Set new score in the hidden text field
+        toUpdate.SetText($"{score}");
+
+        // Animate the CoinTextContainer upwards
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration)
+            .OnComplete(() => StartCoroutine(ResetCoinContainer(score)));
+    }
+
+    private System.Collections.IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        coinTextContainer.localPosition = new Vector3(
+            coinTextContainer.localPosition.x, 
+            containerInitPosition, 
+            coinTextContainer.localPosition.z
+        );
     }
 }
